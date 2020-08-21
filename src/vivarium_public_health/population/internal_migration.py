e01a9365015459ddecd1cf4c1dc19410bbf46853
crangelsmith <camila.rangel.smith@gmail.com>
2020-08-21 12:08:33 +0100

    saving information of old location and assigning new location (#44)

--- src/vivarium_public_health/population/internal_migration.py
+++ src/vivarium_public_health/population/internal_migration.py
@@ -35,7 +35,7 @@ class InternalMigration:
         self.random = builder.randomness.get_stream('outmigtation_handler')
         self.clock = builder.time.clock()
 
-        columns_created = ['internal_outmigration','last_outmigration_time']
+        columns_created = ['internal_outmigration','last_outmigration_time','previous_LAD_locations','previous_MSOA_locations']
         view_columns = columns_created + ['alive', 'age', 'sex', 'location','ethnicity','MSOA']
         self.population_view = builder.population.get_view(view_columns)
         builder.population.initializes_simulants(self.on_initialize_simulants,
@@ -45,13 +45,17 @@ class InternalMigration:
 
     def on_initialize_simulants(self, pop_data):
         pop_update = pd.DataFrame({'internal_outmigration': 'No',
-                                  'last_outmigration_time': pd.NaT},
+                                  'last_outmigration_time': pd.NaT,
+                                   'previous_LAD_locations':[''],
+                                    'previous_MSOA_locations':['']},
                                   index=pop_data.index)
         self.population_view.update(pop_update)
 
     def on_time_step(self, event):
         pop = self.population_view.get(event.index, query="alive =='alive' and sex != 'nan'")
         pop['time_since_last_migration'] = event.time - pop['last_outmigration_time']
+
+        # only allow individuals that have not migrated internaly on the last year.
         pop = pop[(pop['time_since_last_migration']>pd.Timedelta("365 days")) | ((pop['time_since_last_migration'].notnull())==False)]
 
         prob_df = rate_to_probability(pd.DataFrame(self.int_outmigration_rate(pop.index)))
@@ -63,11 +67,15 @@ class InternalMigration:
             int_outmigrated_pop['internal_outmigration'] = pd.Series('Yes', index=int_outmigrated_pop.index)
             int_outmigrated_pop['last_outmigration_time'] = event.time
 
-            test = self.assign_internal_migration(int_outmigrated_pop)
+            int_outmigrated_pop['previous_LAD_locations'] += int_outmigrated_pop['location']
+            int_outmigrated_pop['previous_MSOA_locations'] += int_outmigrated_pop['MSOA']
 
-            print (test)
+            new_MSOA, new_LAD = self.assign_internal_migration(int_outmigrated_pop)
 
-            self.population_view.update(int_outmigrated_pop[['last_outmigration_time', 'internal_outmigration']])
+            int_outmigrated_pop['MSOA'] = new_MSOA
+            int_outmigrated_pop['location'] = new_LAD
+
+            self.population_view.update(int_outmigrated_pop[['last_outmigration_time', 'internal_outmigration','previous_LAD_locations','previous_MSOA_locations','MSOA','location']])
 
     def calculate_outmigration_rate(self, index):
         int_out_migration = self.int_out_migration_rate(index)
@@ -75,6 +83,9 @@ class InternalMigration:
 
     def assign_internal_migration(self,int_migration_pool):
 
+        ''' Based on the characteristic individuals in the internal migration pool, get the relevant
+         migration matrix  assign new locations and save the old ones in a new field
+         '''
 
         int_migration_matrix_rate = self.get_migration_matrix(int_migration_pool)
 
@@ -82,8 +93,10 @@ class InternalMigration:
         # sample the rates for each individual and get the new wards.
         c = int_migration_matrix_rate.cumsum(axis=1)
         u = np.random.rand(len(c), 1)
+        # get the new MSA
         MSOA_choices = (u < c).argmax(axis=1)
 
+        # from the MSOA index get the new MSOA and LAD location name
         MSOA_choices_name = list(map(self.internal_migration_MSOA_location_dict.get, MSOA_choices))
         LAD_choices_name = list(map(self.internal_migration_LAD_location_dict.get, MSOA_choices))
 
@@ -95,16 +108,18 @@ class InternalMigration:
 
         1. Process int_migration_pool (MSOA, SEX, AGE fields) to map dictionary keys
         2. With the dictionary keys read relevant columns
-        3. Return a matrix of n x m where (n is the number of migrants in the pool, m is the number of potential wards they can be assign to).
+        3. Get a matrix of n x m where (n is the number of migrants in the pool, m is the number of potential MSOA they can be assign to).
+        4. Normalise the matrix by total number of counts in each row, to obtain rates
+        4. Return the rate  matrix of n x m
         '''
 
+
         # dummy matrix for now
-        int_migration_matrix= np.random.rand(int_migration_pool.shape[0], len(self.internal_migration_location_dict))
+        int_migration_matrix =  np.random.rand(int_migration_pool.shape[0], len(self.internal_migration_MSOA_location_dict))
         # normalise the matrix to get rates.
-        int_migration_matrix_rate = int_migration_matrix / int_migration_matrix.sum(axis=1)
+        int_migration_matrix_rate = int_migration_matrix / int_migration_matrix.sum(axis=1)[:,None]
 
         return int_migration_matrix_rate
-
 
 
     def __repr__(self):
