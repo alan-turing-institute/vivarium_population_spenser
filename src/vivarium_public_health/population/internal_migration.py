41ca2f8a9fded6e0ca07138254c1871b04faaa8f
kasra-hosseini <khosseini@turing.ac.uk>
2020-08-26 12:23:53 +0100

    Read an OD matrix, perform internal migration

--- src/vivarium_public_health/population/internal_migration.py
+++ src/vivarium_public_health/population/internal_migration.py
@@ -19,24 +19,29 @@ class InternalMigration:
 
     def setup(self, builder):
         int_outmigration_data = builder.data.load("cause.age_specific_internal_outmigration_rate")
+        # Only for testing
+        # mean_value_multiplier = 10.
+        # int_outmigration_data["mean_value"] = int_outmigration_data["mean_value"] * mean_value_multiplier
 
         self.internal_migration_MSOA_location_dict = builder.data.load("internal_migration.MSOA_index")
         self.internal_migration_LAD_location_dict = builder.data.load("internal_migration.LAD_index")
+        self.MSOA_LAD_indices = builder.data.load("internal_migration.MSOA_LAD_indices")
+        self.OD_matrix = builder.data.load("internal_migration.OD_matrix")
 
-        self.int_out_migration_rate = builder.lookup.build_table(int_outmigration_data, key_columns=['sex', 'location', 'ethnicity'],
+        self.int_out_migration_rate = builder.lookup.build_table(int_outmigration_data, 
+                                                                 key_columns=['sex', 'location', 'ethnicity'],
                                                                  parameter_columns=['age', 'year'])
-
 
         self.int_outmigration_rate = builder.value.register_rate_producer('int_outmigration_rate',
                                                                           source=self.calculate_outmigration_rate,
-                                                                          requires_columns=['sex','location','ethnicity'])
-
+                                                                          requires_columns=['sex', 'location', 'ethnicity'])
 
         self.random = builder.randomness.get_stream('outmigtation_handler')
         self.clock = builder.time.clock()
 
-        columns_created = ['internal_outmigration','last_outmigration_time','previous_LAD_locations','previous_MSOA_locations']
-        view_columns = columns_created + ['alive', 'age', 'sex', 'location','ethnicity','MSOA']
+
+        columns_created = ['internal_outmigration', 'last_outmigration_time', 'previous_LAD_locations', 'previous_MSOA_locations']
+        view_columns = columns_created + ['alive', 'age', 'sex', 'location', 'ethnicity', 'MSOA']
         self.population_view = builder.population.get_view(view_columns)
         builder.population.initializes_simulants(self.on_initialize_simulants,
                                                  creates_columns=columns_created)
@@ -45,10 +50,10 @@ class InternalMigration:
 
     def on_initialize_simulants(self, pop_data):
         pop_update = pd.DataFrame({'internal_outmigration': 'No',
-                                  'last_outmigration_time': pd.NaT,
+                                   'last_outmigration_time': pd.NaT,
                                    'previous_LAD_locations':[''],
-                                    'previous_MSOA_locations':['']},
-                                  index=pop_data.index)
+                                   'previous_MSOA_locations':['']},
+                                   index=pop_data.index)
         self.population_view.update(pop_update)
 
     def on_time_step(self, event):
@@ -56,7 +61,7 @@ class InternalMigration:
         pop['time_since_last_migration'] = event.time - pop['last_outmigration_time']
 
         # only allow individuals that have not migrated internaly on the last year.
-        pop = pop[(pop['time_since_last_migration']>pd.Timedelta("365 days")) | ((pop['time_since_last_migration'].notnull())==False)]
+        pop = pop[(pop['time_since_last_migration'] > pd.Timedelta("365 days")) | (pop['time_since_last_migration'].notnull() == False)]
 
         prob_df = rate_to_probability(pd.DataFrame(self.int_outmigration_rate(pop.index)))
         prob_df['No'] = 1-prob_df.sum(axis=1)
@@ -66,7 +71,6 @@ class InternalMigration:
         if not int_outmigrated_pop.empty:
             int_outmigrated_pop['internal_outmigration'] = pd.Series('Yes', index=int_outmigrated_pop.index)
             int_outmigrated_pop['last_outmigration_time'] = event.time
-
             int_outmigrated_pop['previous_LAD_locations'] += int_outmigrated_pop['location']
             int_outmigrated_pop['previous_MSOA_locations'] += int_outmigrated_pop['MSOA']
 
@@ -75,20 +79,25 @@ class InternalMigration:
             int_outmigrated_pop['MSOA'] = new_MSOA
             int_outmigrated_pop['location'] = new_LAD
 
-            self.population_view.update(int_outmigrated_pop[['last_outmigration_time', 'internal_outmigration','previous_LAD_locations','previous_MSOA_locations','MSOA','location']])
+            self.population_view.update(int_outmigrated_pop[['last_outmigration_time', 
+                                                             'internal_outmigration', 
+                                                             'previous_LAD_locations',
+                                                             'previous_MSOA_locations',
+                                                             'MSOA',
+                                                             'location']])
 
     def calculate_outmigration_rate(self, index):
         int_out_migration = self.int_out_migration_rate(index)
         return pd.DataFrame({'internal_outmigration': int_out_migration})
 
     def assign_internal_migration(self,int_migration_pool):
-
         ''' Based on the characteristic individuals in the internal migration pool, get the relevant
          migration matrix  assign new locations and save the old ones in a new field
          '''
-
         int_migration_matrix_rate = self.get_migration_matrix(int_migration_pool)
 
+        # Add a random seed for numpy
+        np.random.seed(64)
 
         # sample the rates for each individual and get the new wards.
         c = int_migration_matrix_rate.cumsum(axis=1)
@@ -105,23 +114,20 @@ class InternalMigration:
     def get_migration_matrix(self,int_migration_pool):
         '''
         Steps to follow
-
-        TODO 1 and 2
-        1. Process int_migration_pool (MSOA, SEX, AGE fields) to map dictionary keys
-        2. With the dictionary keys read relevant columns
-        3. Get a matrix of n x m where (n is the number of migrants in the pool, m is the number of potential MSOA they can be assign to).
-        4. Normalise the matrix by total number of counts in each row, to obtain rates
+        1. Use int_migration_pool to detect the correct OD matrices and rows, this depends on both the origin, sex and age
+        2. This results in a matrix of n x m where (n is the number of migrants in the pool, m is the number of potential MSOA they can be assign to).
+        3. Normalise the matrix by total number of counts in each row, to obtain rates
         4. Return the rate  matrix of n x m
         '''
+        sel_rows = self.MSOA_LAD_indices.merge(int_migration_pool, 
+                                              left_on="Destinations", 
+                                              right_on=["MSOA"])
 
-
-        # dummy matrix for now
-        int_migration_matrix =  np.random.rand(int_migration_pool.shape[0], len(self.internal_migration_MSOA_location_dict)) #(MSOA to MSOA)
-        # normalise the matrix to get rates.
-        int_migration_matrix_rate = int_migration_matrix / int_migration_matrix.sum(axis=1)[:,None]
+        int_migration_matrix = self.OD_matrix[sel_rows.indices.to_list()]
+        # Normalise the matrix to get rates
+        int_migration_matrix_rate = int_migration_matrix / int_migration_matrix.sum(axis=1)[:, None]
 
         return int_migration_matrix_rate
-
 
     def __repr__(self):
         return "InternalMigration()"
--- tests/population/test_internal_migration.py
+++ tests/population/test_internal_migration.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import numpy as np
+import os
 import pandas as pd
 import pytest
+import sys
 from vivarium import InteractiveContext
 from vivarium_public_health.population.spenser_population import TestPopulation, prepare_dataset, transform_rate_table
 from vivarium_public_health.population import InternalMigration
@@ -14,34 +16,29 @@ def config(base_config):
     # change this to you own path
     path_dir= 'persistant_data/'
 
-    # setup emigration rates
-    # read a dataset from daedalus, change columns to be readable by vivarium
-    prepare_dataset(dataset_path="./persistant_data/1000rows_ssm_E08000032_MSOA11_ppp_2011.csv",
-                    output_path="./persistant_data/test_ssm_E08000032_MSOA11_ppp_2011.csv"
-                    )
-
-    # file should have columns -> PID,location,sex,age,ethnicity
-    filename_pop = 'test_ssm_E08000032_MSOA11_ppp_2011.csv'
-
     # file should have columns -> PID,location,sex,age,ethnicity
     #filename_pop = 'Testfile.csv'
+    filename_pop = 'test_ssm_E08000032_MSOA11_ppp_2011.csv'
+
+    # setup emigration rates
+    # read a dataset from daedalus, change columns to be readable by vivarium
+    prepare_dataset(dataset_path=os.path.join(path_dir, "1000rows_ssm_E08000032_MSOA11_ppp_2011.csv"),
+                    output_path=os.path.join(path_dir, filename_pop))
 
     filename_internal_outmigration_name = 'InternalOutmig2011_LEEDS2.csv'
     path_to_internal_outmigration_file = "{}/{}".format(path_dir, filename_internal_outmigration_name)
 
-    filename_total_population = 'MY2011AGEN.csv'
-    path_to_total_population_file = "{}/{}".format(path_dir, filename_total_population)
-
     path_to_pop_file= "{}/{}".format(path_dir,filename_pop)
-
     pop_size = len(pd.read_csv(path_to_pop_file))
 
-    base_config.update({
+    path_msoa_to_lad = os.path.join(path_dir, 'Middle_Layer_Super_Output_Area__2011__to_Ward__2016__Lookup_in_England_and_Wales.csv')
+    path_to_OD_matrix = os.path.join(path_dir, "OD_matrix_EW_no_intra_flows.csv")
 
+    base_config.update({
         'path_to_pop_file':path_to_pop_file,
         'path_to_internal_outmigration_file': path_to_internal_outmigration_file,
-        'path_to_total_population_file': path_to_total_population_file,
-
+        'path_msoa_to_lad': path_msoa_to_lad,
+        'path_to_OD_matrix': path_to_OD_matrix,
         'population': {
             'population_size': pop_size,
             'age_start': 0,
@@ -53,11 +50,7 @@ def config(base_config):
         }, source=str(Path(__file__).resolve()))
     return base_config
 
-
-
 def test_internal_outmigration(config, base_plugins):
-    # start_population_size = config.population.population_size
-
     num_days = 365*5
     components = [TestPopulation(), InternalMigration()]
     simulation = InteractiveContext(components=components,
@@ -65,40 +58,46 @@ def test_internal_outmigration(config, base_plugins):
                                     plugin_configuration=base_plugins,
                                     setup=False)
 
-
-
     df = pd.read_csv(config.path_to_internal_outmigration_file)
 
     # to save time, only look at locations existing on the test dataset.
-    df_internal_outmigration = df[(df['LAD.code'] == 'E08000032')]
+    df_internal_outmigration = df[df['LAD.code'].isin(['E08000032', 
+                                                       'E08000033', 
+                                                       'E08000034',
+                                                       'E06000024',
+                                                       'E08000035',
+                                                       'E07000163'])]
+    #df_internal_outmigration = df.copy()
 
     asfr_data = transform_rate_table(df_internal_outmigration, 2011, 2012, config.population.age_start,
                                      config.population.age_end)
-
     simulation._data.write("cause.age_specific_internal_outmigration_rate", asfr_data)
 
+    # Read MSOA ---> LAD
+    msoa_lad_df = pd.read_csv(config.path_msoa_to_lad)
+    # Read OD matrix, only destinations
+    OD_matrix_dest = pd.read_csv(config.path_to_OD_matrix, usecols=["Destinations"])
+    OD_matrix_with_LAD = OD_matrix_dest[1:].merge(msoa_lad_df[["MSOA11CD", "LAD16CD"]], 
+                                                  left_on="Destinations", 
+                                                  right_on="MSOA11CD")
+    OD_matrix_with_LAD["indices"] = OD_matrix_with_LAD.index
+    if not OD_matrix_dest["Destinations"][1:].to_list() == OD_matrix_with_LAD["Destinations"].to_list():
+        sys.exit("[ERROR] indices of MSOA and LAD do not match. Contact the developers.")
+    
+    # Create indices for MSOA and LAD
+    MSOA_location_index = OD_matrix_with_LAD["Destinations"].to_dict()
+    LAD_location_index = OD_matrix_with_LAD["LAD16CD"].to_dict()
 
-    # create dictionary that connects the migration matrix index to the location id
-    MSOA_location_index = {}
-    LAD_location_index = {}
-    lad_msoa_df = pd.read_csv('persistant_data/Middle_Layer_Super_Output_Area__2011__to_Ward__2016__Lookup_in_England_and_Wales.csv')
+    # Now, read the whole matrix (if it passes the first check)
+    OD_matrix_rd = pd.read_csv(config.path_to_OD_matrix)
+    OD_matrix = OD_matrix_rd[OD_matrix_rd.columns[2:]][1:].to_numpy().astype(np.float)
 
-    # for simplicity in this test lets allow the migrants to only move between MSOAs in the same LAD
-    lad_msoa_df = lad_msoa_df[lad_msoa_df['LAD16CD']=='E08000032']
-
-    count = 0
-    for i in np.unique(lad_msoa_df['MSOA11CD']):
-        MSOA_location_index[count] = i
-        LAD_location_index[count] = lad_msoa_df[lad_msoa_df['MSOA11CD']==i]['LAD16CD'].values[0]
-        count = count + 1
-
-    # save the index data into the component
     simulation._data.write("internal_migration.MSOA_index", MSOA_location_index)
     simulation._data.write("internal_migration.LAD_index", LAD_location_index)
-
+    simulation._data.write("internal_migration.OD_matrix", OD_matrix)
+    simulation._data.write("internal_migration.MSOA_LAD_indices", OD_matrix_with_LAD)
 
     simulation.setup()
-
 
     simulation.run_for(duration=pd.Timedelta(days=num_days))
     pop = simulation.get_population()
@@ -110,4 +109,3 @@ def test_internal_outmigration(config, base_plugins):
 
     assert len(pop[pop['last_outmigration_time']!='NaT']) > 0, 'time of out migration gets saved.'
     assert len(pop[pop['previous_MSOA_locations']!='']) > 0, 'previous location of the migrant gets saved.'
-
